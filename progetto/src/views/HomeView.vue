a6edfbc13d8821c273849bdc05c9d80b7a82cf4d
LauraCnn99 <lauracannazza@gmail.com>
2024-09-22 14:42:56 +0200

    Aggiunta html a HomeView

--- progetto/src/views/HomeView.vue
+++ progetto/src/views/HomeView.vue
@@ -1,7 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Zuhair Murad's logo" src="../assets/logo.webp" />
+    <HelloWorld msg="Welcome to Zuhair Murad fashion shows' web app " />
+  </div>
+    <div class="col-md-6">
+    <div class="card mx auto" style="width: 80%">
+      <div id="carosellosfilate" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="assets/sfilata-1.jpg"
+              class="d-block w-100"
+              alt="sfilata1"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="assets/sfilata-2.jpg"
+              class="d-block w-100"
+              alt="sfilata2"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="assets/sfilata-3.jpg"
+              class="d-block w-100"
+              alt="sfilata3"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="assets/sfilata-4.jpg"
+              class="d-block w-100"
+              alt="sfilata4"
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carosellosfilate"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carosellosfilate"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div class="card-body" style="text-align:center">
+        <h3 class="card-title">- Fashion shows' list -</h3>
+        <h5 class="card-text" style="margin:3%">SPRING 2025</h5>
+        <h5 class="card-text" style="margin:3%">FALL 2024</h5>
+        <h5 class="card-text" style="margin:3%">SPRING 2024</h5>
+        <h5 class="card-text" style="margin:3%">FALL 2023</h5>
+      </div>
+    </div>
   </div>
 </template>
 
